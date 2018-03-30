190d9b52a28b9f5aaf81fa01775cab2320ebdb15
khanhpn <khanhpham@bravesoft.vn>
2018-03-30 15:44:27 +0900

    issue 3

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -1,49 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ColorPicker from './components/ColorPicker';
-import SizeSetting from './components/SizeSetting';
-import Result from './components/Result';
-import Reset from './components/Reset';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      color: 'red',
-      fontSize: 15
-    };
-  }
-  onSetColor = (params) => {
-    this.setState({
-      color: params
-    })
-  }
-
-  onChangeFontSize = (params) => {
-    this.setState({
-      fontSize: this.state.fontSize + params
-    });
-  }
-
-  onReset = () => {
-    this.setState({
-      color: "red",
-      fontSize: 15
-    })
-  }
-
   render() {
     return (
       <div className="container">
-        <div className="row">
-          <ColorPicker color={this.state.color} onReceiveColor={this.onSetColor}/>
-          <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            <SizeSetting font={this.state.fontSize} changeFontSize={this.onChangeFontSize}/>
-            <Reset onReset={this.onReset} />
-          </div>
-          <Result color={this.state.color} font={this.state.fontSize}/>
-        </div>
       </div>
     )
   }
